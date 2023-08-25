53682ca9da9590158180eedae266bbe33feee78d
Simon Prickett <simon@redislabs.com>
2023-08-25 15:57:48 +0100

    Now creates the index programmatically.

--- data_loader.py
+++ data_loader.py
@@ -15,18 +15,24 @@ redis_client = redis.from_url(os.getenv("REDIS_URL"))
 
 # Drop any existing index.
 try:
+    print("Checking for previous index and dropping if found.")
     redis_client.ft("idx:regions").dropindex(delete_documents = False)
 except:
     # Dropping an index that doesn't exist throws an exception 
     # but isn't an error in this case - we just want to start
     # from a known point.
+
+    # TODO check for error... see trains example
     pass
 
-# TODO CREATE NEW INDEX WITH GENERIC COMMAND APPROACH.
 # Create a new index.
-# ft.create idx:regions on json prefix 1 region: schema $.name as name tag $.boundaries as boundaries geoshape spherical $.forecast.wind as wind text $.forecast.sea as sea text $.forecast.weather as weather text $.forecast.visibility as visibility text 
+print("Creating index.")
+
+redis_client.execute_command("FT.CREATE", "idx:regions", "ON", "JSON", "PREFIX", "1", "region:", "SCHEMA", "$.name", "AS", "name", "TAG", "$.boundaries", "AS", "boundaries", "GEOSHAPE", "SPHERICAL", "$.forecast.wind", "AS", "WIND", "TEXT", "$.forecast.sea", "AS", "sea", "TEXT", "$.forecast.weather", "AS", "weather", "TEXT", "$.forecast.visibility", "AS", "visibility", "TEXT")
 
 # Load the shipping forecast regional data from the JSON file.
+# TODO make filename configurable
+# TODO rewrite without ijson dependency
 num_loaded = 0
 
 with open ("data/shipping_forecast_regions.json", "rb") as input_file:
@@ -40,4 +46,7 @@ with open ("data/shipping_forecast_regions.json", "rb") as input_file:
             num_loaded += 1
             print(f"Stored {redis_key} ({region['name']})")
 
+redis_client.quit()
+
 print(f"Regions loaded: {num_loaded}")
+print("Done!")
